485d2765e924da2b4f9a6a69213197a6c0bf8f0d
Darshan <darshanpatel2923@gmail.com>
2023-10-09 16:31:16 +0530

    Adding a new app similar to recipe app

--- Recipe_App/Frontend/src/pages/Home.jsx
+++ Recipe_App/Frontend/src/pages/Home.jsx
@@ -1,8 +1,37 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import axios from 'axios';
 
 const Home = () => {
+    const [recipes, setRecipes] = useState([]);
+
+    useEffect(() => {
+        const fetchRecipe = async () => {
+            try {
+                const recipe = await axios.get("http://localhost:3001/recipes");
+                setRecipes(recipe.data);
+            } catch (err) {
+                console.error(err);
+            }
+        }
+        fetchRecipe();
+    }, [])
+
     return (
-        <div>Home</div>
+        <div className='home-section'>
+            <h1 className="text-center">Welcome to Recipe App</h1><br />
+            <ul>
+                {recipes.map((recipe) => (
+                    <li key={recipe._id}>
+                        <div>
+                            <h2>{recipe.name}</h2>
+                            <div className='instructions'><p> {recipe.instructions}</p></div>
+                            <div><img src={recipe.imageUrl} alt="recipe-name" /></div>
+                            <p>Cooking Time: {recipe.cookingTime} (minutes)</p>
+                        </div>
+                    </li>
+                ))};
+            </ul>
+        </div>
     )
 }
 
