9e10bb03d4fed71cc41e22074b2cff03d3527313
Darshan <darshanpatel2923@gmail.com>
2024-12-06 16:44:17 +0530

    Added Threejs for react and created navbar and working on hero section for dummy portfolio

---
+++ Basic_Threejs_Tut/vite_react/src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import Navbar from './sections/Navbar'
+import Hero from './sections/Hero'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <main className="max-w-7xl mx-auto relative">
+        <Navbar />
+        <Hero />
+      </main>
+    </>
+  )
+}
+
+export default App
