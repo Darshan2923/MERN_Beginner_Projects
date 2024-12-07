e74608419f8fce534ecc534b68714999b0681403
Darshan <darshanpatel2923@gmail.com>
2024-12-07 12:44:26 +0530

    Added About and projects section for our website

--- Basic_Threejs_Tut/vite_react/src/App.jsx
+++ Basic_Threejs_Tut/vite_react/src/App.jsx
@@ -1,6 +1,8 @@
 import { useState } from 'react'
 import Navbar from './sections/Navbar'
 import Hero from './sections/Hero'
+import About from './sections/About'
+import Projects from './sections/Projects'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -10,6 +12,8 @@ function App() {
       <main className="max-w-7xl mx-auto relative">
         <Navbar />
         <Hero />
+        <About />
+        <Projects />
       </main>
     </>
   )
