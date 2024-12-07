cb14347550e9d5356ad3a2ec27f8676cf9668954
Darshan <darshanpatel2923@gmail.com>
2024-12-07 15:30:20 +0530

    Created contact me, client testimonials and footer section for our project

--- Basic_Threejs_Tut/vite_react/src/App.jsx
+++ Basic_Threejs_Tut/vite_react/src/App.jsx
@@ -3,6 +3,9 @@ import Navbar from './sections/Navbar'
 import Hero from './sections/Hero'
 import About from './sections/About'
 import Projects from './sections/Projects'
+import Clients from './sections/Clients'
+import Contact from './sections/Contact'
+import Footer from './sections/Footer'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -14,6 +17,9 @@ function App() {
         <Hero />
         <About />
         <Projects />
+        <Clients />
+        <Contact />
+        <Footer />
       </main>
     </>
   )
