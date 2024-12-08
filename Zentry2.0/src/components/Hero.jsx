0c20efe2742040dfd617108b596190ff0fa501cd
Darshan <darshanpatel2923@gmail.com>
2024-12-08 12:52:59 +0530

    Added button as an reusable component for our app...wil do hero section later

--- Zentry2.0/src/components/Hero.jsx
+++ Zentry2.0/src/components/Hero.jsx
@@ -1,4 +1,6 @@
 import React, { useRef, useState } from 'react'
+import Button from './Button';
+import { TiLocationArrow } from 'react-icons/ti';
 
 const Hero = () => {
     const [currentIndex, setCurrentIndex] = useState(1);
@@ -24,16 +26,7 @@ const Hero = () => {
 
     return (
         <div className='relative h-dvh w-screen overflow-x-hidden'>
-            <div id="video-frame" className='relative z-10 h-dvh w-screen overflow-hidden rounded-lg bg-blue-75'>
-                <div>
-                    <div className="mask-clip-path absolute-center absolute z-50 size-64 cursor-pointer overflow-hidden rounded-lg">
-                        <div onClick={handleMiniVdClick} className='origin-center'>
-                            <video src={getVideoSrc(currentIndex + 1)} ref={nextVdRef} loop muted id='current-video' className='size-64 origin-center scale-150 object-cover object-center scale-50 opacity-0 transition-all duration-500 ease-in hover:scale-100 hover:opacity-100' onLoadedData={handleVideoLoad} />
-                        </div>
 
-                    </div>
-                </div>
-            </div>
         </div>
     )
 }
