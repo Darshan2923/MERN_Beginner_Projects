3f56900727a80c58a86b53fb0797df310437dc32
Darshan <darshanpatel2923@gmail.com>
2025-09-13 09:51:02 +0530

    Test 2 after Github actions failed

--- GitHubActions/test.js
+++ GitHubActions/test.js
@@ -2,6 +2,4 @@ console.log("Test");
 setTimeout(() => {
     console.log("Test after 2 seconds");
 }, 2000);
-console.log('====================================');
 console.log("Hello from test.js");
-console.log('====================================');
