fe1521aa83f0f697617ada90f058be52010cd327
Darshan <darshanpatel2923@gmail.com>
2025-09-14 12:50:36 +0530

    Completed the api with proper linting, testing and formatting to proper structure. Also implemented CI/CD pipelines to automate the flow

--- InfinityAPI/src/app.js
+++ InfinityAPI/src/app.js
@@ -6,6 +6,7 @@ import cors from 'cors';
 import cookieParser from 'cookie-parser';
 import authRoutes from '#routes/auth.routes.js';
 import securityMiddleware from '#middleware/security.middleware.js';
+import userRoutes from '#routes/users.routes.js';
 
 const app = express();
 
@@ -15,28 +16,40 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
-app.use(morgan('combined', {
+app.use(
+  morgan('combined', {
     stream: {
-        write: (message) => logger.info(message.trim()),
+      write: message => logger.info(message.trim()),
     },
-}))
+  })
+);
 
 app.use(securityMiddleware);
 
 app.get('/', (req, res) => {
-    logger.info('Root endpoint hit');
-    res.status(200).send('Hello from acquisitions API!');
+  logger.info('Root endpoint hit');
+  res.status(200).send('Hello from acquisitions API!');
 });
 
 app.get('/health', (req, res) => {
-    res.status(200).json({ status: 'OK', timestamp: new Date().toISOString(), uptime: process.uptime() });
+  res
+    .status(200)
+    .json({
+      status: 'OK',
+      timestamp: new Date().toISOString(),
+      uptime: process.uptime(),
+    });
 });
 
 app.get('/api', (req, res) => {
-    res.status(200).json({ message: "Acquisitions API is running" });
+  res.status(200).json({ message: 'Acquisitions API is running!' });
 });
 
 app.use('/api/auth', authRoutes); //api/auth/sign-in
+app.use('/api/users', userRoutes);
 
+app.use((req, res) => {
+  res.status(404).json({ error: 'Route not found' });
+});
 
 export default app;
